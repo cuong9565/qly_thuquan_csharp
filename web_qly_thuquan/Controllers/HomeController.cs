a50fc8f64164280a1125a3a68f1f25fb22641416
cuong9565 <cuongmanhle0905@gmail.com>
2025-05-02 22:54:37 +0700

    Cuong update ChangePassword Web_View

--- web_qly_thuquan/Controllers/HomeController.cs
+++ web_qly_thuquan/Controllers/HomeController.cs
@@ -14,5 +14,25 @@ namespace web_qly_thuquan.Controllers
             if (Session["UserId"] == null) return RedirectToAction("Index", "Login");
             return View();
         }
+        public ActionResult DatCho()
+        {
+            if (Session["UserId"] == null) return RedirectToAction("Index", "Login");
+            return View();
+        }
+        public ActionResult LichSu()
+        {
+            if (Session["UserId"] == null) return RedirectToAction("Index", "Login");
+            return View();
+        }
+        public ActionResult TaiKhoan()
+        {
+            if (Session["UserId"] == null) return RedirectToAction("Index", "Login");
+            return View();
+        }
+        public ActionResult DoiMatKhau()
+        {
+            if (Session["UserId"] == null) return RedirectToAction("Index", "Login");
+            return View();
+        }
     }
 }
