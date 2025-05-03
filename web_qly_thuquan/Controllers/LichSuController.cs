028927ba4d8153f2bc69adaffdafe525aefbeaa2
cuong9565 <cuongmanhle0905@gmail.com>
2025-05-03 14:22:57 +0700

    Cuong add view LichSu

---
+++ web_qly_thuquan/Controllers/LichSuController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_qly_thuquan.Controllers
+{
+    public class LichSuController : Controller
+    {
+        // GET: LichSu
+        public ActionResult Vao()
+        {
+            if (Session["UserId"] == null) return RedirectToAction("Index", "Login");
+            return View();
+        }
+        public ActionResult DatCho()
+        {
+            if (Session["UserId"] == null) return RedirectToAction("Index", "Login");
+            return View();
+        }
+        public ActionResult ViPham()
+        {
+            if (Session["UserId"] == null) return RedirectToAction("Index", "Login");
+            return View();
+        }
+    }
+}
